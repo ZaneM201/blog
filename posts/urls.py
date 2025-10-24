d954ccbd99b4a6c626d028ae7413bd483d879414
Zane Moon <zanem201@gmail.com>
2025-10-23 20:38:52 -0700

    113 session 2

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    
+    path("", PostListView.as_view(), name="posts_list"),
+    path("detailed/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
+class PostListView(ListView):
+    """
+
+    """
+    template_name = "posts/list.html"
+    model = Post
+    context_object_name = "posts"
+
+class PostDetailView(DetailView):
+    """
+
+    """
+    template_name = "posts/detail.html"
+    model = Post
+    context_object_name = "detailed"
